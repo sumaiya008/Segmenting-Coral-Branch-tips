8b499a29ab0f52cbbc69ac76ac903faa3365d3a3
sumaiya008 <suddin008@citymail.cuny.edu>
2023-09-01 13:40:18 -0400

    inital commit

---
+++ CCNY-DSE-Capstone-Project-Segmenting-Coral-Branch-tips/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CCNY-DSE-Capstone-Project-Segmenting-Coral-Branch-tips',
+    author='JamesHerlan/SumaiyaUddin',
+    license='',
+)
